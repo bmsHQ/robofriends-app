9e0bf529bb7ea24208f61b2e7e0e52f822359e3b
Jovan Banks <bmshq@icloud.com>
2023-09-14 17:46:39 -0500

    added error boundaries to components, added packages to deploy app on gh-pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../component/CardList";
 import Searchbox from "../component/Searchbox";
 import Scroll from "../component/Scroll";
+import ErrorBoundary from "../component/ErrorBoundary";
 import "../containers/App.css";
 
 class App extends Component {
@@ -35,7 +36,9 @@ class App extends Component {
                 <h1 className="f1">RoboFriends</h1>
                 <Searchbox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots={filteredRobots} />
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundary>
                 </Scroll>
             </div>
         );
